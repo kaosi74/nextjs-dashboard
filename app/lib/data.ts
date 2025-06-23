bf4bdb28fb59ab7494e7ab8e98a82077f0dd12ec
Kosi74 <kaosinwankwo74@gmail.com>
2025-06-23 14:28:51 +0100

    integrated user authentication using nextauth, middleware for communication between server and client

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -11,7 +11,7 @@ import { formatCurrency } from './utils';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
-export async function fetchRevenue() {
+export async function fetchRevenue(id: string) {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
@@ -21,7 +21,7 @@ export async function fetchRevenue() {
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    console.log('Data fetch completed after 3 seconds.');
+    // console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
